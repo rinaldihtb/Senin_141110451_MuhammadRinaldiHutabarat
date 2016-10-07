2a4245940460c9775618326adab169c7a69b72be
Muhammad Rinaldi Hutabarat <rinaldi@rinaldi.com>
2016-10-08 01:47:48 +0700

    Latihan_2_1

---
+++ Latihan_2_1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Latihan_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.tgl = new System.Windows.Forms.DomainUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bln = new System.Windows.Forms.DomainUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 2);
+            this.monthCalendar1.FirstDayOfWeek = System.Windows.Forms.Day.Monday;
+            this.monthCalendar1.Location = new System.Drawing.Point(28, 58);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // tgl
+            // 
+            this.tgl.Items.Add("1");
+            this.tgl.Items.Add("2");
+            this.tgl.Items.Add("3");
+            this.tgl.Items.Add("4");
+            this.tgl.Items.Add("5");
+            this.tgl.Items.Add("6");
+            this.tgl.Items.Add("7");
+            this.tgl.Items.Add("8");
+            this.tgl.Items.Add("9");
+            this.tgl.Items.Add("10");
+            this.tgl.Items.Add("11");
+            this.tgl.Items.Add("12");
+            this.tgl.Items.Add("13");
+            this.tgl.Items.Add("14");
+            this.tgl.Items.Add("15");
+            this.tgl.Items.Add("16");
+            this.tgl.Items.Add("17");
+            this.tgl.Items.Add("18");
+            this.tgl.Items.Add("19");
+            this.tgl.Items.Add("20");
+            this.tgl.Items.Add("21");
+            this.tgl.Items.Add("22");
+            this.tgl.Items.Add("23");
+            this.tgl.Items.Add("24");
+            this.tgl.Items.Add("25");
+            this.tgl.Items.Add("26");
+            this.tgl.Items.Add("27");
+            this.tgl.Items.Add("28");
+            this.tgl.Items.Add("29");
+            this.tgl.Items.Add("30");
+            this.tgl.Items.Add("31");
+            this.tgl.Location = new System.Drawing.Point(28, 19);
+            this.tgl.Margin = new System.Windows.Forms.Padding(10);
+            this.tgl.MaximumSize = new System.Drawing.Size(120, 0);
+            this.tgl.Name = "tgl";
+            this.tgl.Size = new System.Drawing.Size(42, 20);
+            this.tgl.TabIndex = 1;
+            this.tgl.Text = "Pilih Tanggal";
+            this.tgl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tgl.SelectedItemChanged += new System.EventHandler(this.tgl_SelectedItemChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 19);
+            this.button1.Margin = new System.Windows.Forms.Padding(10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tambah";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bln
+            // 
+            this.bln.Items.Add("Januari");
+            this.bln.Items.Add("Februari");
+            this.bln.Items.Add("Maret");
+            this.bln.Items.Add("April");
+            this.bln.Items.Add("Mei");
+            this.bln.Items.Add("Juni");
+            this.bln.Items.Add("Juli");
+            this.bln.Items.Add("Agustus");
+            this.bln.Items.Add("September");
+            this.bln.Items.Add("Oktober");
+            this.bln.Items.Add("November");
+            this.bln.Items.Add("Desember");
+            this.bln.Location = new System.Drawing.Point(77, 19);
+            this.bln.Margin = new System.Windows.Forms.Padding(10);
+            this.bln.Name = "bln";
+            this.bln.Size = new System.Drawing.Size(119, 20);
+            this.bln.TabIndex = 1;
+            this.bln.Text = "Pilih Bulan";
+            this.bln.SelectedItemChanged += new System.EventHandler(this.bln_SelectedItemChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 399);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bln);
+            this.Controls.Add(this.tgl);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DomainUpDown tgl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DomainUpDown bln;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Latihan_2_1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            deklarasiSabtuMinggu();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Deklarasi Tanggal Lahir
+            DateTime tgl_lahir = new DateTime(1996, 09, 19);
+            monthCalendar1.AddBoldedDate(tgl_lahir);
+            bln.SelectedIndex = 0;
+            tgl.SelectedIndex = 0;
+        }
+
+        public void deklarasiSabtuMinggu()
+        {
+            //Deklarasi awal Tahun
+            DateTime awalTahun = new DateTime(2016, 1, 1);
+            DateTime DateTemp = awalTahun;
+            //monthCalendar1.SetDate(awalTahun);
+
+            //label1.Text = Convert.ToInt32(awalTahun.DayOfWeek).ToString();
+            //Deklarasi Hari sabtu dan Minggu
+            int k = 0;
+            int sisak = 0;
+            for (int i = 1; i < 13; i++)
+            {
+                DateTemp = new DateTime(2016, i, 1);
+                k = 6 - (int)DateTemp.DayOfWeek;
+                sisak = (int)DateTemp.DayOfWeek + k;
+                DateTemp = DateTemp.AddDays(k);
+                //label1.Text = sisak.ToString();
+                //break;
+                if (sisak == 6)
+                {
+
+                    for (int j = 1; j < 6; j++)
+                    {
+
+                        //label1.Text = DateTemp.ToString();
+                        monthCalendar1.AddBoldedDate(DateTemp);
+                        DateTemp = DateTemp.AddDays(7);
+                    }
+                }
+
+                DateTemp = new DateTime(2016, i, 1);
+                k = 7 - (int)DateTemp.DayOfWeek;
+                sisak = (int)DateTemp.DayOfWeek + k;
+                DateTemp = DateTemp.AddDays(k);
+                //label1.Text = sisak.ToString();
+                //break;
+                if (sisak == 7)
+                {
+
+                    for (int j = 1; j < 6; j++)
+                    {
+
+                        //label1.Text = DateTemp.ToString();
+                        monthCalendar1.AddBoldedDate(DateTemp);
+                        DateTemp = DateTemp.AddDays(7);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int blns = bln.SelectedIndex;
+            blns++;
+            //label1.Text = tgl.Text + " - " +blns;
+            DateTime dateAnnual = new DateTime(2016, blns , Convert.ToInt32(tgl.Text));
+            label1.Text = dateAnnual.ToString();
+            monthCalendar1.AddAnnuallyBoldedDate(dateAnnual);
+        }
+
+        private void bln_SelectedItemChanged(object sender, EventArgs e)
+        {
+            int indexBln = bln.SelectedIndex;
+            indexBln++;
+            //label1.Text = indexBln.ToString();
+        }
+
+        private void tgl_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
