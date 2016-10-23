927680be37a4f2c787e44dfd54afcf574e589487
Rinaldi <rinaldihtb@ymail.com>
2016-10-24 01:26:51 +0700

    Latihan_4_1

---
+++ Latihan_4_1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Latihan_4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.italicChange = new System.Windows.Forms.Button();
+            this.underlineChange = new System.Windows.Forms.Button();
+            this.boldChange = new System.Windows.Forms.Button();
+            this.ColorChange = new System.Windows.Forms.ComboBox();
+            this.FfamilyChange = new System.Windows.Forms.ComboBox();
+            this.fontChange = new System.Windows.Forms.ComboBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(9, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(487, 240);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // italicChange
+            // 
+            this.italicChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.italicChange.Location = new System.Drawing.Point(137, 32);
+            this.italicChange.Name = "italicChange";
+            this.italicChange.Size = new System.Drawing.Size(21, 21);
+            this.italicChange.TabIndex = 9;
+            this.italicChange.Text = "I";
+            this.italicChange.UseVisualStyleBackColor = true;
+            this.italicChange.Click += new System.EventHandler(this.italicChange_Click);
+            // 
+            // underlineChange
+            // 
+            this.underlineChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.underlineChange.Location = new System.Drawing.Point(164, 32);
+            this.underlineChange.Name = "underlineChange";
+            this.underlineChange.Size = new System.Drawing.Size(21, 21);
+            this.underlineChange.TabIndex = 10;
+            this.underlineChange.Text = "U";
+            this.underlineChange.UseVisualStyleBackColor = true;
+            this.underlineChange.Click += new System.EventHandler(this.underlineChange_Click);
+            // 
+            // boldChange
+            // 
+            this.boldChange.AllowDrop = true;
+            this.boldChange.Location = new System.Drawing.Point(110, 32);
+            this.boldChange.Name = "boldChange";
+            this.boldChange.Size = new System.Drawing.Size(21, 21);
+            this.boldChange.TabIndex = 11;
+            this.boldChange.Text = "B";
+            this.boldChange.UseVisualStyleBackColor = true;
+            this.boldChange.Click += new System.EventHandler(this.boldChange_Click);
+            // 
+            // ColorChange
+            // 
+            this.ColorChange.FormattingEnabled = true;
+            this.ColorChange.Location = new System.Drawing.Point(344, 32);
+            this.ColorChange.Name = "ColorChange";
+            this.ColorChange.Size = new System.Drawing.Size(155, 21);
+            this.ColorChange.TabIndex = 6;
+            this.ColorChange.Text = "Black";
+            this.ColorChange.SelectedIndexChanged += new System.EventHandler(this.ColorChange_SelectedIndexChanged);
+            // 
+            // FfamilyChange
+            // 
+            this.FfamilyChange.FormattingEnabled = true;
+            this.FfamilyChange.Location = new System.Drawing.Point(191, 32);
+            this.FfamilyChange.Name = "FfamilyChange";
+            this.FfamilyChange.Size = new System.Drawing.Size(147, 21);
+            this.FfamilyChange.TabIndex = 7;
+            this.FfamilyChange.Text = "Arial";
+            this.FfamilyChange.SelectedIndexChanged += new System.EventHandler(this.FfamilyChange_SelectedIndexChanged);
+            // 
+            // fontChange
+            // 
+            this.fontChange.FormattingEnabled = true;
+            this.fontChange.Location = new System.Drawing.Point(12, 31);
+            this.fontChange.Name = "fontChange";
+            this.fontChange.Size = new System.Drawing.Size(92, 21);
+            this.fontChange.TabIndex = 8;
+            this.fontChange.Text = "8 pt";
+            this.fontChange.SelectedIndexChanged += new System.EventHandler(this.fontChange_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(503, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 302);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.italicChange);
+            this.Controls.Add(this.underlineChange);
+            this.Controls.Add(this.boldChange);
+            this.Controls.Add(this.ColorChange);
+            this.Controls.Add(this.FfamilyChange);
+            this.Controls.Add(this.fontChange);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button italicChange;
+        private System.Windows.Forms.Button underlineChange;
+        private System.Windows.Forms.Button boldChange;
+        private System.Windows.Forms.ComboBox ColorChange;
+        private System.Windows.Forms.ComboBox FfamilyChange;
+        private System.Windows.Forms.ComboBox fontChange;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+    }
+}
+
---
+++ Latihan_4_1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Latihan_4_1
+{
+    public partial class Form1 : Form
+    {
+        private bool boldV = false;
+        private bool italicV = false;
+        private bool underlineV = false;
+
+        private string firstText;
+        public Form1()
+        {
+            InitializeComponent();
+            this.firstText = "";
+        }
+
+        private void underlineChange_Click(object sender, EventArgs e)
+        {
+            this.underlineV = true;
+            //this.BIU();
+            this.changeFontSize();
+            this.underlineV = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            int i = 8;
+            while (i < 72)
+            {
+                if (i <= 11)
+                    i++;
+                else
+                {
+                    i += 3;
+                }
+                fontChange.Items.Add(i + " pt");
+
+            }
+
+            foreach (FontFamily ffs in FontFamily.Families)
+            {
+                FfamilyChange.Items.Add(ffs.Name);
+            }
+
+            foreach (System.Reflection.PropertyInfo Props in typeof(Color).GetProperties())
+            {
+                if (Props.PropertyType.FullName == "System.Drawing.Color")
+                {
+                    ColorChange.Items.Add(Props.Name);
+                }
+                //comboBox3.Items.Add(Props.Name);
+                //comboBox3.Items.Add(Props.PropertyType.FullName);
+            }
+        }
+
+        private void fontChange_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.changeFontSize();
+            //int size = Convert.ToInt32(fontChange.Text.Split(' ')[0]);
+            //richTextBox1.SelectionFont = new System.Drawing.Font(richTextBox1.SelectionFont.FontFamily, size);
+        }
+
+        private void FfamilyChange_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.changeFontSize();
+            /*int size = Convert.ToInt32(fontChange.Text.Split(' ')[0]);
+            
+            richTextBox1.SelectionFont = new System.Drawing.Font(FfamilyChange.Text, size, richTextBox1.SelectionFont.Style);*/
+        }
+
+        private void ColorChange_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionColor = Color.FromName(ColorChange.Text);
+        }
+
+        private void boldChange_Click(object sender, EventArgs e)
+        {
+            this.boldV = true;
+            //this.BIU();
+            this.changeFontSize();
+            this.boldV = false;
+        }
+
+        private void italicChange_Click(object sender, EventArgs e)
+        {
+            this.italicV = true;
+            //this.BIU();
+            this.changeFontSize();
+            this.italicV = false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void changeFontSize()
+        {
+            int size = Convert.ToInt32(fontChange.Text.Split(' ')[0]);
+            int PosisiAwal = richTextBox1.SelectionStart;
+            int PosisiAkhir = richTextBox1.SelectionLength;
+            for (int i = PosisiAwal; i < PosisiAwal + PosisiAkhir; i++)
+            {
+                //SET PENENTUAN POSISI yang akan diganti perHURUF
+                richTextBox1.SelectionStart = i;
+                richTextBox1.SelectionLength = 1;
+                this.BIU();
+                richTextBox1.SelectionFont = new System.Drawing.Font(FfamilyChange.Text, size, richTextBox1.SelectionFont.Style);
+            }
+            richTextBox1.SelectionStart = PosisiAwal;
+            richTextBox1.SelectionLength = PosisiAkhir;
+            /*richTextBox1.SelectionFont = new System.Drawing.Font(FfamilyChange.Text, size, richTextBox1.SelectionFont.Style);*/
+        }
+
+        private void BIU()
+        {
+            Font slf, slfNew;
+            slf = richTextBox1.SelectionFont;
+            //UNDERLINE
+            if (underlineV)
+            {
+                if (slf.Underline)
+                    slfNew = new Font(richTextBox1.Font, slf.Style & ~FontStyle.Underline);
+                else
+                    slfNew = new Font(slf, slf.Style | FontStyle.Underline);
+                richTextBox1.SelectionFont = slfNew;
+            }
+            //BOLD
+            if (boldV)
+            {
+                if (slf.Bold)
+                    slfNew = new Font(slf, slf.Style & ~FontStyle.Bold);
+                else
+                    slfNew = new Font(slf, slf.Style | FontStyle.Bold);
+                richTextBox1.SelectionFont = slfNew;
+            }
+            //ITALIC
+            if (italicV)
+            {
+                if (slf.Italic)
+                    slfNew = new Font(slf, slf.Style & ~FontStyle.Italic);
+                else
+                    slfNew = new Font(slf, slf.Style | FontStyle.Italic);
+                richTextBox1.SelectionFont = slfNew;
+            }
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+                this.firstText = richTextBox1.Text;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            this.save();
+        }
+
+        private void save()
+        {
+            saveFileDialog1.Filter = "All Files (*.*)|*.*| Text Document (*.txt)|*.txt|Rich Textbox Format (*.rft)|*.rft";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+            }
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (richTextBox1.Text != this.firstText)
+            {
+                DialogResult check = MessageBox.Show("Save changes ?", "My Application", MessageBoxButtons.YesNoCancel);
+                if (check == DialogResult.Yes)
+                {
+                    this.save();
+                }
+                else if (check == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+    }
+}
