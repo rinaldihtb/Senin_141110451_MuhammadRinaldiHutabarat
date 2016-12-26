278c8a403358ea8462aca28a3a6c440cecf9c9cf
Rinaldi <rinaldihtb@ymail.com>
2016-12-26 07:20:06 +0700

    Database_POS(Insert Costumer dan Edit)

--- Database_POS/Costumer.Designer.cs
+++ Database_POS/Costumer.Designer.cs
@@ -30,18 +30,30 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.save_barang_button = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.simpan_barang = new System.Windows.Forms.Button();
+            this.nokostumer = new System.Windows.Forms.TextBox();
+            this.alamatkostumer = new System.Windows.Forms.TextBox();
+            this.namakostumer = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.barang_views = new System.Windows.Forms.DataGridView();
+            this.Update_Kostumer = new System.Windows.Forms.Button();
+            this.Enokostumer = new System.Windows.Forms.TextBox();
+            this.Ealamatkostumer = new System.Windows.Forms.TextBox();
+            this.Enamakostumer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Eidkostumer = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cek_costumer = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.barang_views)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,18 +61,18 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, 243);
+            this.tabControl1.Location = new System.Drawing.Point(8, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(487, 208);
+            this.tabControl1.Size = new System.Drawing.Size(487, 237);
             this.tabControl1.TabIndex = 2;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.save_barang_button);
-            this.tabPage1.Controls.Add(this.textBox3);
-            this.tabPage1.Controls.Add(this.textBox2);
-            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.simpan_barang);
+            this.tabPage1.Controls.Add(this.nokostumer);
+            this.tabPage1.Controls.Add(this.alamatkostumer);
+            this.tabPage1.Controls.Add(this.namakostumer);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.label2);
@@ -68,40 +80,41 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(479, 182);
+            this.tabPage1.Size = new System.Drawing.Size(479, 211);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Tambah Customer";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // save_barang_button
+            // simpan_barang
             // 
-            this.save_barang_button.Location = new System.Drawing.Point(334, 87);
-            this.save_barang_button.Name = "save_barang_button";
-            this.save_barang_button.Size = new System.Drawing.Size(93, 41);
-            this.save_barang_button.TabIndex = 3;
-            this.save_barang_button.Text = "Simpan";
-            this.save_barang_button.UseVisualStyleBackColor = true;
+            this.simpan_barang.Location = new System.Drawing.Point(334, 87);
+            this.simpan_barang.Name = "simpan_barang";
+            this.simpan_barang.Size = new System.Drawing.Size(93, 41);
+            this.simpan_barang.TabIndex = 3;
+            this.simpan_barang.Text = "Simpan";
+            this.simpan_barang.UseVisualStyleBackColor = true;
+            this.simpan_barang.Click += new System.EventHandler(this.save_barang_button_Click);
             // 
-            // textBox3
+            // nokostumer
             // 
-            this.textBox3.Location = new System.Drawing.Point(152, 135);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(115, 20);
-            this.textBox3.TabIndex = 2;
+            this.nokostumer.Location = new System.Drawing.Point(152, 135);
+            this.nokostumer.Name = "nokostumer";
+            this.nokostumer.Size = new System.Drawing.Size(115, 20);
+            this.nokostumer.TabIndex = 2;
             // 
-            // textBox2
+            // alamatkostumer
             // 
-            this.textBox2.Location = new System.Drawing.Point(152, 98);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(115, 20);
-            this.textBox2.TabIndex = 2;
+            this.alamatkostumer.Location = new System.Drawing.Point(152, 98);
+            this.alamatkostumer.Name = "alamatkostumer";
+            this.alamatkostumer.Size = new System.Drawing.Size(115, 20);
+            this.alamatkostumer.TabIndex = 2;
             // 
-            // textBox1
+            // namakostumer
             // 
-            this.textBox1.Location = new System.Drawing.Point(152, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(115, 20);
-            this.textBox1.TabIndex = 2;
+            this.namakostumer.Location = new System.Drawing.Point(152, 59);
+            this.namakostumer.Name = "namakostumer";
+            this.namakostumer.Size = new System.Drawing.Size(115, 20);
+            this.namakostumer.TabIndex = 2;
             // 
             // label4
             // 
@@ -135,16 +148,27 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(114, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Tambah Barang";
+            this.label1.Text = "Tambah Customer";
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.Eidkostumer);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.cek_costumer);
+            this.tabPage2.Controls.Add(this.Update_Kostumer);
+            this.tabPage2.Controls.Add(this.Enokostumer);
+            this.tabPage2.Controls.Add(this.Ealamatkostumer);
+            this.tabPage2.Controls.Add(this.Enamakostumer);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label8);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(479, 182);
+            this.tabPage2.Size = new System.Drawing.Size(479, 211);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Edit Costumer";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -154,16 +178,110 @@
             this.barang_views.AllowUserToAddRows = false;
             this.barang_views.AllowUserToDeleteRows = false;
             this.barang_views.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.barang_views.Location = new System.Drawing.Point(0, -3);
+            this.barang_views.Location = new System.Drawing.Point(8, 255);
             this.barang_views.Name = "barang_views";
-            this.barang_views.Size = new System.Drawing.Size(483, 240);
+            this.barang_views.Size = new System.Drawing.Size(483, 199);
             this.barang_views.TabIndex = 3;
+            this.barang_views.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.barang_views_CellContentClick);
+            // 
+            // Update_Kostumer
+            // 
+            this.Update_Kostumer.Location = new System.Drawing.Point(371, 149);
+            this.Update_Kostumer.Name = "Update_Kostumer";
+            this.Update_Kostumer.Size = new System.Drawing.Size(93, 41);
+            this.Update_Kostumer.TabIndex = 11;
+            this.Update_Kostumer.Text = "Simpan";
+            this.Update_Kostumer.UseVisualStyleBackColor = true;
+            this.Update_Kostumer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Enokostumer
+            // 
+            this.Enokostumer.Location = new System.Drawing.Point(156, 160);
+            this.Enokostumer.Name = "Enokostumer";
+            this.Enokostumer.Size = new System.Drawing.Size(115, 20);
+            this.Enokostumer.TabIndex = 8;
+            // 
+            // Ealamatkostumer
+            // 
+            this.Ealamatkostumer.Location = new System.Drawing.Point(156, 123);
+            this.Ealamatkostumer.Name = "Ealamatkostumer";
+            this.Ealamatkostumer.Size = new System.Drawing.Size(115, 20);
+            this.Ealamatkostumer.TabIndex = 9;
+            // 
+            // Enamakostumer
+            // 
+            this.Enamakostumer.Location = new System.Drawing.Point(156, 84);
+            this.Enamakostumer.Name = "Enamakostumer";
+            this.Enamakostumer.Size = new System.Drawing.Size(115, 20);
+            this.Enamakostumer.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Nomor HP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Alamat";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Nama Lengkap";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(118, 20);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Edit Costumer";
+            // 
+            // Eidkostumer
+            // 
+            this.Eidkostumer.Location = new System.Drawing.Point(156, 48);
+            this.Eidkostumer.Name = "Eidkostumer";
+            this.Eidkostumer.Size = new System.Drawing.Size(115, 20);
+            this.Eidkostumer.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(47, 51);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "ID Costumer";
+            // 
+            // cek_costumer
+            // 
+            this.cek_costumer.Location = new System.Drawing.Point(288, 48);
+            this.cek_costumer.Name = "cek_costumer";
+            this.cek_costumer.Size = new System.Drawing.Size(58, 20);
+            this.cek_costumer.TabIndex = 11;
+            this.cek_costumer.Text = "Cek";
+            this.cek_costumer.UseVisualStyleBackColor = true;
+            this.cek_costumer.Click += new System.EventHandler(this.cek_costumer_Click);
             // 
             // Costumer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 457);
+            this.ClientSize = new System.Drawing.Size(500, 466);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.barang_views);
             this.Name = "Costumer";
@@ -172,6 +290,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.barang_views)).EndInit();
             this.ResumeLayout(false);
 
@@ -181,15 +301,26 @@
 
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Button save_barang_button;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button simpan_barang;
+        private System.Windows.Forms.TextBox nokostumer;
+        private System.Windows.Forms.TextBox alamatkostumer;
+        private System.Windows.Forms.TextBox namakostumer;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataGridView barang_views;
+        private System.Windows.Forms.TextBox Eidkostumer;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button cek_costumer;
+        private System.Windows.Forms.Button Update_Kostumer;
+        private System.Windows.Forms.TextBox Enokostumer;
+        private System.Windows.Forms.TextBox Ealamatkostumer;
+        private System.Windows.Forms.TextBox Enamakostumer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- Database_POS/Costumer.cs
+++ Database_POS/Costumer.cs
@@ -13,13 +13,17 @@ namespace Database_POS
 {
     public partial class Costumer : Form
     {
+        // Database Connection
         MySqlConnection con = new MySqlConnection("server=127.0.0.1;database=project;Uid=root;Pwd=");
+        MySqlDataAdapter DA;
+        DataTable DT = new DataTable();
+        MySqlCommand query;
+        public string ID_costumer;
 
         public Costumer()
         {
-            
-
             InitializeComponent();
+            this.view_barang();
         }
 
         private void Costumer_Load(object sender, EventArgs e)
@@ -28,13 +32,120 @@ namespace Database_POS
         
         }
 
-        public void show_list()
+        public void view_barang()
         {
-            /*job = new MySqlCommand("select * from customer", con);
-            DA = new MySqlDataAdapter(job);
+            query = new MySqlCommand("select * from customer", con);
+            DA = new MySqlDataAdapter(query);
             DT = new DataTable();
             DA.Fill(DT);
-            list_customer_view.DataSource = DT;*/
+            barang_views.DataSource = DT;
+        }
+
+        private void barang_views_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void save_barang_button_Click(object sender, EventArgs e)
+        {
+            string nama = this.namakostumer.Text;
+            string alamat = this.alamatkostumer.Text;
+            string nohp = this.nokostumer.Text;
+            DateTime datenow = getdatetime();
+
+
+            query = new MySqlCommand("INSERT INTO customer VALUES(@id,@nama,@alamat, @nohp, @gender, @dateinsert, @dateupdate)", con);
+            query.Parameters.AddWithValue("@id", null);
+            query.Parameters.AddWithValue("@nama", nama);
+            query.Parameters.AddWithValue("@alamat", alamat);
+            query.Parameters.AddWithValue("@nohp", nama);
+            query.Parameters.AddWithValue("@gender", null);
+            query.Parameters.AddWithValue("@dateinsert", datenow);
+            query.Parameters.AddWithValue("@dateupdate", null);
+
+            try
+            {
+                con.Open();
+                query.ExecuteNonQuery();
+                MessageBox.Show("Berhasil Input Costumer");
+                con.Close();
+                view_barang();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        public static DateTime getdatetime()
+        {
+            DateTime getdatetime = DateTime.UtcNow;
+            int year = getdatetime.Year;
+            int month = getdatetime.Month;
+            int day = getdatetime.Day;
+            int hour = getdatetime.Hour;
+            int min = getdatetime.Minute;
+            int sec = getdatetime.Second;
+            DateTime datetime = new DateTime(year, month, day, hour, min, sec);
+            return datetime;
+        }
+
+        private void cek_costumer_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string id_param = this.Eidkostumer.Text;
+            //MessageBox.Show(id_param);
+            query = new MySqlCommand("SELECT * FROM customer WHERE ID=@kode", con);
+            query.Parameters.AddWithValue("@kode", id_param);
+            MySqlDataReader Reader = query.ExecuteReader();
+
+            if (!Reader.HasRows)
+            {
+                con.Close();
+                MessageBox.Show("Data Tidak Ditemukan");
+                return;
+            }
+            if (Reader.Read())
+            {
+                this.Enamakostumer.Text = Reader["Nama"].ToString();
+                this.Ealamatkostumer.Text = Reader["Alamat"].ToString();
+                this.Enokostumer.Text = Reader["NoHp"].ToString();
+            }
+            Reader.Close();
+            con.Close();
+            //MessageBox.Show(DA..ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nama = this.Enamakostumer.Text;
+            this.ID_costumer = this.Eidkostumer.Text;
+            string alamat = this.Ealamatkostumer.Text;
+            string nohp = this.Enokostumer.Text;
+            DateTime datenow = getdatetime();
+
+
+            query = new MySqlCommand("UPDATE customer SET Nama=@nama, Alamat=@alamat, NoHp=@nohp, Editied_at=@dateupdate WHERE ID=@id", con);
+            query.Parameters.AddWithValue("@id", this.ID_costumer);
+            query.Parameters.AddWithValue("@nama", nama);
+            query.Parameters.AddWithValue("@alamat", alamat);
+            query.Parameters.AddWithValue("@nohp", nohp);
+            query.Parameters.AddWithValue("@dateupdate", datenow);
+
+            try
+            {
+                con.Open();
+                query.ExecuteNonQuery();
+                MessageBox.Show("Berhasil Update Costumer");
+                con.Close();
+                view_barang();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
         }
     }
 }
